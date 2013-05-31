226d38754203980ae6e3629e16484b08fdf51a65
Manuel Aguirre <programador.manuel@gmail.com>
2013-05-31 12:20:40 -0430

    agregando funciones para facilitar la creacion de filtros en el Builder

--- MapperBuilder.php
+++ MapperBuilder.php
@@ -25,6 +25,44 @@ class MapperBuilder
         return $this;
     }
 
+    public function sanitizeString($item)
+    {
+        return $this->add($item, array(FILTER_SANITIZE_STRING));
+    }
+
+    public function sanitizeInt($item, array $options = array(), $flags = null)
+    {
+        $options = array(
+            'options' => $options,
+            'flags' => $flags,
+        );
+        return $this->add($item, array(FILTER_SANITIZE_NUMBER_INT => $options));
+    }
+
+    public function sanitizeFloat($item, array $options = array(), $flags = FILTER_FLAG_ALLOW_FRACTION)
+    {
+        $options = array(
+            'options' => $options,
+            'flags' => $flags,
+        );
+        return $this->add($item, array(FILTER_SANITIZE_NUMBER_FLOAT => $options));
+    }
+
+    public function sanitizeEmail($item)
+    {
+        return $this->add($item, array(FILTER_SANITIZE_EMAIL));
+    }
+
+    public function sanitizeUrl($item)
+    {
+        return $this->add($item, array(FILTER_SANITIZE_URL));
+    }
+
+    public function callback($item, $callback)
+    {
+        return $this->add($item, array(FILTER_CALLBACK => array('options' => $callback)));
+    }
+
     /**
      * 
      * @param Item $item
